f991b1838c62eb607896a11c1323f231fd2a36ce
rafaelfv27 <rafael.fv07@gmail.com>
2024-06-06 12:34:06 -0300

    new way to get charges

--- pacKing.py
+++ pacKing.py
@@ -27,7 +27,6 @@ if len(argv) < 2:
     print('An example of the .inp file was created in the current directory.')
     exit(1)
 
-
 class pacKing:
     def __init__(self):
         self.charges = []
@@ -37,14 +36,15 @@ class pacKing:
         self.preparing_data()
         self.read_box()
         self.modify=self.change_charges()
+        self.calculateDensity()
         self.print_file()
         
-    
     def control(self):
         arq = open(argv[1], 'r').readlines()
         self.solves = []
         self.solutes = []
         self.numbers = []
+        self.molarMass = []
         self.atomnumbers = []
 
         for i in range(len(arq)):
@@ -56,23 +56,60 @@ class pacKing:
                 self.numbers.append(int(arq[i].split()[3]))
             if 'data' in arq[i]:
                 self.data = arq[i].split()[2]
+            if 'box_size' in arq[i]:
+                self.box_size = arq[i].split()[2]
+
+        if len(self.solves) == 0:
+            print('No solvent was found in the input file.')
+            exit(1) 
+        
+        if len(self.solutes) == 0:
+            print('No solute was found in the input file.')
+            
+
+        try:
+            self.data
+        except:
+            print('No data file was found in the input file.')
+            exit(1)
+
+        try:
+            self.box_size
+        except:
+            print('No box_size was found in the input file.')
+            exit(1)            
         
         for i in range(len(self.solves)):
             print('Solvent {}: Number of Molecules {}'.format(self.solves[i].split('.')[0], self.numbers[i]))
         for i in range(len(self.solutes)):
             print('Solute {}: Number of Molecules {}'.format(self.solutes[i].split('.')[0], self.numbers[i+len(self.solves)]))
 
+    def calculateDensity(self):
+        mol_voly = 0
 
+        for i in range(len(self.solves)):
+            mol_voly += 1.6605577881 * sum(self.molarMass[i] * self.numbers[i] for i in range(len(self.solves)))
+        for i in range(len(self.solutes)):
+            mol_voly += 1.6605577881 * sum(self.molarMass[i] * self.numbers[i] for i in range(len(self.solutes)))
+        
+        print('Box Size: {} Angstrom'.format(self.box_size))
+        print('Molecular Volume: {} cm³'.format(mol_voly))
+        print('Density: {} g/cm³'.format(mol_voly/(float(self.box_size)**3)))
+        
+        
+                
     def preparing_data(self):
         for i in range(len(self.solves)):
             self.gaussian_log = open(self.solves[i], 'r').readlines()
             self.atomnumbers.append(len(G16LOGfile(self.solves[i]).getMolecule()))
+            self.molarMass.append(G16LOGfile(self.solves[i]).getMolecule().getMM())
             self.charges.append(self.get_charges())
             self.charges_dict = {}
 
         for i in range(len(self.solutes)):
             self.gaussian_log = open(self.solutes[i], 'r').readlines()
             self.atomnumbers.append(len(G16LOGfile(self.solutes[i]).getMolecule()))
+            self.molarMass.append(G16LOGfile(self.solutes[i]).getMolecule().getMM())
             self.charges.append(self.get_charges())
             self.charges_dict = {}
 
@@ -98,11 +135,9 @@ class pacKing:
         list_of_molecules = self.solves + self.solutes
         count = 1
         for i in range(len(list_of_molecules)):            
-            for j in range(start, start + self.numbers[i]*self.atomnumbers[i]):
-                c = self.box[j].split()[8] + '_' + str(count)
-                print(c, count)
-                test += ('{} {} {} {} {} {} {} {} {}\n'.format(self.box[j].split()[0], self.box[j].split()[1], self.box[j].split()[2], self.charges[i][c], self.box[j].split()[4], self.box[j].split()[5], self.box[j].split()[6], self.box[j].split()[7], self.box[j].split()[8]))
-                
+            for j in range(start, start + self.numbers[i] * self.atomnumbers[i]):
+                c = list(self.charges[i].keys())[count-1]
+                test += self.box[j].replace(self.box[j].split()[3], self.charges[i][c])
                 count += 1
                 if count > self.atomnumbers[i]:
                     count = 1
@@ -110,16 +145,18 @@ class pacKing:
             start = j + 1
         return test
 
-                
+
     def get_charges(self):
+        start_list = []
+        end_list = []
         for i in range(len(self.gaussian_log)):
             if 'ESP charges:' in self.gaussian_log[i]:
-                start = i + 2
+                start_list.append(i + 2)
             if 'Sum of ESP charges' in self.gaussian_log[i]:
-                end = i 
-                break
-        for i in range(start, end):
+                end_list.append(i)
+        for i in range(start_list[-1], end_list[-1]):
             self.charges_dict[self.gaussian_log[i].split()[1]+'_'+self.gaussian_log[i].split()[0]] = self.gaussian_log[i].split()[2]
+        print(self.charges_dict)  
         return self.charges_dict
     
     
@@ -128,9 +165,14 @@ class pacKing:
         arq = open('new_'+self.data, 'w')
         arqv = open(self.data, 'r').readlines()
         for i in arqv[0:self.start]:
-            arq.write(i)
-
-        
+            if 'xlo' in i.split():
+                arq.write('{} {} xlo xhi\n'.format(-float(self.box_size)/2, float(self.box_size)/2))
+            elif 'ylo' in i.split():
+                arq.write('{} {} ylo yhi\n'.format(-float(self.box_size)/2, float(self.box_size)/2))
+            elif 'zlo' in i.split():
+                arq.write('{} {} zlo zhi\n'.format(-float(self.box_size)/2, float(self.box_size)/2))
+            else:
+                arq.write(i)
         arq.write(self.modify)
         for i in arqv[self.end + 1:]:
             arq.write(i)
--- packmol.py
+++ packmol.py
@@ -1,6 +1,11 @@
 from sys import argv
 
 
+
+if len (argv) <= 1:
+    print('Usage: python packmol.py <Solute> <Solvent> <Solvent> ...')
+    exit()
+
 class packmol():
 
 
@@ -14,27 +19,62 @@ class packmol():
         self.filetype = input('Filetype: ')
         self.output = input('Output Name: ')
         self.structures= [x for x in argv[1:]]
-        self.center = str(input('Is one of the Molecules at the center of the box? (y/n)'))
-        self.choose_center = input('Which molecule is at the center of the box? (1,2,3,4,5,6,7,8,9,10)')
-        self.number_mol = int(input('Number of molecules: '))
+        print('The following structures will be used:', self.structures)
+        print('Molecule {} will be considered a solute'.format(self.structures[0]))
+        self.center = (lambda x: True if x == 'y' else False)(input('Is Solute at the center of the box? (y/n): ').lower())
+        # self.choose_center = input('Which molecule is at the center of the box?')
+        self.number_mol_solute = int(input('Number of solute molecules: '))
+        self.number_mol_sovent = int(input('Number of solvent molecules: '))
         self.box_coordinates_x = float(input('X coordinate of box: '))
-        self.box_coordinates_y = float(input('Y coordinate of box: '))
+        self.box_coordinates_y = float(input('Y coordinate of box: ')) 
         self.box_coordinates_z = float(input('Z coordinate of box: '))
 
 
     def write_data(self, file):
 
+
         with open(file, 'w') as file:
             file.write('tolerance {}\n'.format(self.tolerance))
             file.write('randominitialpoint\n')
+            file.write('seed 1 \n')
             file.write('filetype {} \n'.format(self.filetype))
             file.write('output {}.{} \n'.format(self.output, self.filetype))
             print(self.structures)
-            for i in self.structures:
-                if self.center == 'y':
-                    file.write('structure {}\n'.format(argv[1]))
-                    file.write('number {} \n'.format(self.number_mol))
-                    file.write('inside box {} {} {} '.format(self.box_coordinates_x /2 ,self.box_coordinates_y /2,self.box_coordinates_z /2))
+
+            
+            for i in range(len(self.structures)):
+                if self.center == True:
+                    if i == 0:
+                        file.write('\nstructure {}\n'.format(self.structures[i]))
+                        file.write('    number {} \n'.format(self.number_mol_solute/self.number_mol_solute))
+                        file.write('    inside box 0. 0. 0. 0. 0. 0. \n')
+                        file.write('    centerofmass\n')
+                        file.write('    radius 1.5 \n')
+                        file.write('end structure\n')
+                    else:
+                        self.box_coordinates_x -= 0.25
+                        self.box_coordinates_y -= 0.25
+                        self.box_coordinates_z -= 0.25
+                        file.write('\nstructure {}\n'.format(self.structures[i]))
+                        file.write('    number {} \n'.format(self.number_mol_sovent))
+                        file.write('    inside box {} {} {} {} {} {} \n'.format(-self.box_coordinates_x/2, -self.box_coordinates_y /2,-self.box_coordinates_z /2, self.box_coordinates_x /2, self.box_coordinates_y /2, self.box_coordinates_z /2 ))
+                        file.write('end structure\n')
+
+                else:
+                    if i == 0:
+                        file.write('\nstructure {}\n'.format(self.structures[i]))
+                        file.write('    number {} \n'.format(self.number_mol_solute))
+                        file.write('    inside box 0. 0. 0. {} {} {} \n'.format(self.box_coordinates_x, self.box_coordinates_y ,self.box_coordinates_z))
+                        file.write('end structure\n')
+                    if self.structures[i] != self.structures[0]:
+                        self.box_coordinates_x -= 0.25
+                        self.box_coordinates_y -= 0.25
+                        self.box_coordinates_z -= 0.25
+                        file.write('\nstructure {}\n'.format(self.structures[i]))
+                        file.write('    number {} \n'.format(self.number_mol_sovent))
+                        file.write('    inside box 0. 0. 0. {} {} {} \n'.format(self.box_coordinates_x, self.box_coordinates_y ,self.box_coordinates_z))
+                        file.write('end structure\n')
+
 
 
 
